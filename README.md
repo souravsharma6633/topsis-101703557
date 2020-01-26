5ed663614f5e8ca3aa27297f0043b9cde4a0d003
souravsharma6633 <60296733+souravsharma6633@users.noreply.github.com>
2020-01-26 14:22:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+This package is implementation of topsis technique of multi-criteria decision analysis.
+This package will accept three parameters:
+1. data.csv //file which contains the models and parameters
+2. string of weights separated by commas(,)
+3. string of requirements (+/-) separated by commas(,)
+// important
+install pandas,sys and math libraries before installing this
+//
+You can install this package using following command
+pip install topsis-101703550
--- setup.py
+++ setup.py
@@ -1,37 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 25 22:17:52 2020
-
-@author: sharma._.g
-"""
-
 from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
 setup(
-  name = 'topsis_101703557',         # How you named your package folder (MyLib)
-  packages = ['topsis_101703557'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Implementation of topsis. User should install pandas, sys, math and operator libraries prior to execution of topsis. Command line example to run is: python topsis_101703557.py data.csv "1,2,3,4" "+,+,-,+". ',   # Give a short description about your library
-  author = 'sharma._.g',                   # Type in your name
-  author_email = 'souravsharma6633@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/souravsharma6633/topsis_101703557',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/souravsharma6633/topsis_101703557/archive/0.8.tar.gz',    # I explain this later on
-  keywords = ['HAVE', 'A', 'GOOD','DAY'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'pandas',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
-  include_package_data=True,
-  entry_points={
+    name="topsis_101703557",
+    version="1.0",
+    description="A Python package to rank ML models/choices using topsis technique",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/souravsharma6633/topsis_101703557",
+    author="Sourav Sharma",
+    author_email="souravsharma6633@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_101703557"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
         "console_scripts": [
             "topsis_101703557=topsis_101703557.__init__:main",
         ]
